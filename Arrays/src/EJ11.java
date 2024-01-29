47be76059e6cbc15bada45bb382e5855df561cd6
Paul1Luca <paul.luca@educa.madrid.org>
2024-01-29 14:44:57 +0100

    Ejercicios Arrays.

--- Arrays/src/EJ11.java
+++ Arrays/src/EJ11.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class EJ11 {
     public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduce el tamaño de la lista");
+        int dimension = teclado.nextInt();
 
+        int[] numeros = new int[dimension];
+
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = (int)(Math.random() * 30 + 0);
+            System.out.printf("%d, ", numeros[i]);
+        }
     }
 }
