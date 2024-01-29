3b40e36f1d73002fe0aec2bbf2c5e38f7bed4ec2
Paul1Luca <paul.luca@educa.madrid.org>
2024-01-29 13:06:29 +0100

    Ejercicios Arrays.

--- Arrays/src/EJ3.java
+++ Arrays/src/EJ3.java
@@ -1,13 +1,23 @@
+import java.util.Scanner;
+
 public class EJ3 {
     public static void main(String[] args) {
         int[] numeros = new int[15];
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduce 15 números");
 
         for (int i = 0; i < numeros.length; i++) {
-            numeros[i] = (int)(Math.random()*15 + 1);
+            numeros[i] = teclado.nextInt();
         }
 
-        for (int i = numeros.length - 1; i >= 0; i--) {
-            System.out.println("Valores = " + numeros[i]);
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.printf("%d ", numeros[i]);
+        }
+
+        System.out.printf("%n");
+
+        for (int i = 14; i >=0 ; i--) {
+            System.out.printf("%d ", numeros[i]);
         }
     }
 }
--- Arrays/src/EJ4.java
+++ Arrays/src/EJ4.java
@@ -1,22 +1,22 @@
 public class EJ4 {
     public static void main(String[] args) {
-        int[][] numeros = new int[10][10];
+        int[][] bidimensional = new int[10][10];
 
-        for (int i = 0; i < numeros.length; i++) {
-            for (int j = 0; j < numeros[i].length; j++) {
-                if (i % 2 == 0) {
-                    numeros[i][j] = 1;
+        for (int i = 0; i < bidimensional.length; i++) {
+            for (int j = 0; j < bidimensional[i].length; j++) {
+                if (i%2 == 0) {
+                    bidimensional[i][j] = 1;
                 } else {
-                    numeros[i][j] = 0;
+                    bidimensional[i][j] = 0;
                 }
             }
-
         }
-        for (int i = 0; i < numeros.length; i++) {
-                for (int j = 0; j < numeros[i].length; j++) {
-                    System.out.printf("%d", numeros[i][j]);
-                }
-            System.out.printf("\n");
+
+        for (int i = 0; i < bidimensional.length; i++) {
+            for (int j = 0; j < bidimensional[i].length; j++) {
+                System.out.printf("%d ", bidimensional[i][j]);
+            }
+            System.out.printf("%n");
         }
     }
 }
